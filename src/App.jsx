00a82c4d6b75dd076a26a45227d1faa9b72554c6
Githubshakil <shakilmahmud007@gmail.com>
2025-08-06 19:47:53 +0600

    update
    page design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ToDoList from './components/ToDoList'
+import ThemeChange from './components/ThemeChange'
 
 const App = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
     <ToDoList/>
+    <ThemeChange/>
     </div>
   )
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const ToDoList = () => {
   return (
-    <div className='flex flex-col items-center space-y-4'>
-        <h2 className='text-xl font-bold'>Todo List App</h2>
-        <div className='flex space-x-2'>
-            <input
-             type="text"
-             placeholder='add a todo'
-             className='px-4 py-2 border border-gray-300 rounded placeholder:text-gray-400 text-black' />
-             <button className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 '>Add</button>
-        </div>
+    <div className="flex flex-col items-center space-y-4">
+      <h2 className="text-xl font-bold">Todo List App</h2>
+      <div className="flex space-x-2">
+        <input
+          type="text"
+          placeholder="add a todo"
+          className="px-4 py-2 border border-gray-300 rounded placeholder:text-gray-400 text-black"
+        />
+        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 ">
+          Add
+        </button>
+      </div>
 
-
-        <div>
-            <ul className='w-full max-w-md'>
-                <li className='flex justify-between items-center px-4 py-2 border-b space-x-4'>
-                    <span>items 1</span>
-                    <button className='text-red-500 hover:underline'>Remove</button>
-                </li>
-                <li>items 2</li>
-                <li>items 3</li>
-                <li>items 4</li>
-            </ul>
-        </div>
+      <div>
+        <ul className="w-full max-w-md">
+          <li className="flex justify-start items-center px-4 py-2 border-b space-x-4">
+            <span>items 1</span>
+            <button className="text-red-500 hover:underline">Remove</button>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
